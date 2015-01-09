c73a5e611c9731de3c4b049d32f053542b7a9213
John <john@johnharris.tv>
2015-01-09 16:53:02 -0600

    Added UpperCase function

--- server.py
+++ server.py
@@ -12,6 +12,35 @@ def index():
            <ul><li>http://localhost:8080/up?s=type_your_string_here</li>\
            <li>http://localhost:8080/up?URL=http://url_to_file.txt</li></ul>"
 
+@route('/up')
+def uppercase():
+    """ 
+    Convert given text to uppercase
+    (as a plain argument, or from a textfile's URL)
+    Returns an indented JSON structure
+    """
+    
+    # Store HTTP GET arguments
+    plain_text   = request.GET.get('s'  , default=None)
+    textfile_url = request.GET.get('URL', default=None)
+
+    # Execute WebService specific task
+    # here, converting a string to upper-casing
+    if plain_text is not None:
+        return json.dumps(
+            {'input' : plain_text, 
+             'result': plain_text.upper()
+             },
+            indent=4)
+
+    elif textfile_url is not None:
+        textfile = urlopen(textfile_url).read()
+        return json.dumps(
+            {'input' : textfile,
+             'output': '\n'.join([line.upper() for line in textfile.split('\n')]) 
+             },
+            indent=4)
+
 
 if __name__ == '__main__':
     # To run the server, type-in $ python server.py
